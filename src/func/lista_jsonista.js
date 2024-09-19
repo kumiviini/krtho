f574563e3e1778181a1f8e556a5bb915f32d650e
viinikumi <miikka.vainionp@gmail.com>
2024-09-19 11:34:04 +0300

    ymp muuttuja ja vite config

--- src/func/lista_jsonista.js
+++ src/func/lista_jsonista.js
@@ -1,6 +1,14 @@
-import ylakierto from "./ylakierto.json";
+//import ylakierto from "./ylakierto.json";
+
+const ylakiertoJson = process.env.REACT_APP_YLAKIERTO;
+const ylakierto = JSON.parse(ylakiertoJson || "[]");
 
 export const lista_jsonista = () => {
+  console.log(
+    "Ympäristömuuttuja REACT_APP_YLAKIERTO:",
+    process.env.REACT_APP_YLAKIERTO
+  );
+
   const valilista = [];
 
   for (var i = 0; i < ylakierto.length; i++) {
